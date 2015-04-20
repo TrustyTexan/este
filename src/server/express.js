704d3b12baf51823c70599f24a3ebc73c2eabe68
David Salgado <dsalgado@spredfast.com>
2015-04-20 12:16:41 -0500

    Fixed direct routes other than / (home)

--- src/server/express.js
+++ src/server/express.js
@@ -10,7 +10,7 @@ export default function () {
   app.use(compression());
   app.use('/', express.static(path.join(__dirname, '/../../')));
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/../../'));
   });
 
